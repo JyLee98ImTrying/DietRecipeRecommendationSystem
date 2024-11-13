a00fc1a93c19e9c573aaf483b69a963b870ef1bb
Lee Jie Yeng <109010589+JyLee98ImTrying@users.noreply.github.com>
2024-11-13 21:52:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
-import io
 
 def load_data():
     try:
