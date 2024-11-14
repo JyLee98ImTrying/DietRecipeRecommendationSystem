671a5969ccf81ecc9b76f35ac7392aaf2d5984d5
Lee Jie Yeng <109010589+JyLee98ImTrying@users.noreply.github.com>
2024-11-14 21:18:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def load_data():
     try:
         # URL of the raw CSV file from GitHub
         # Note: Use the raw GitHub URL instead of the repository page URL
-        url = 'https://raw.githubusercontent.com/JyLee98ImTrying/DietRecipeRecommendationSystem/master/df_sample (1).csv'
+        url = 'https://raw.githubusercontent.com/JyLee98ImTrying/DietRecipeRecommendationSystem/master/df_sample.csv'
         
         df = pd.read_csv(url, delimiter=',', encoding='utf-8', on_bad_lines='skip')
         
